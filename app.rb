6de82313107555a0386462ed11e1ee8bf83449b2
Taavi Repän <trepan@gmail.com>
2011-06-21 19:13:34 +0300

    Slight tuning

--- app.rb
+++ app.rb
@@ -40,7 +40,10 @@ def get_spectrum(q)
 end
 
 get '/view' do
-  @header = "Search results"
+  @header = "<form action='/view'>"
+  @header += "<input type='text' name='query' value='#{params[:query]}'>"
+  @header += "<input type='submit' value='Search'>"
+  @header += "</form>"
   
   s = get_spectrum(params[:query])
   @json = s.to_json
@@ -48,18 +51,11 @@ get '/view' do
   @main = "<canvas id='spectrum' width='800' height='200'>" 
   @main += "Your browser doesn't support <b>canvas</b> element."
   @main += "</canvas><br>"
-  @main += "<input id='minL' value='380'>..<input value='780' id='maxL'> nm<br>";
-  @main += "Exposure: <input id='exp' value='1.0'><input value='Draw' type='button'><br>";
+  @main += "<input id='minL' value='380' size='4'>..<input value='780' id='maxL' size='4'> nm<br>";
+  @main += "Brightness: <input id='exp' value='1.0' size='4'><br>";
+  @main += "<input value='Draw' width='160' type='button'><br>";
   @main += "<div id='info'></div>"
 
   erb :index
 end
 
-get '/debug' do
-  post_data = {:spectra => "Hg I", :low_wl => 380, :upp_wl => 780, :unit => 1,
-               :show_av => 4, :allowed_out => 1, :forbid_out => 1,
-               :intens_out => 1, :show_obs_wl => 1}
-  res = Net::HTTP.post_form(URI.parse("http://physics.nist.gov/cgi-bin/ASD/lines1.pl"), post_data)
-
-  res.body
-end
